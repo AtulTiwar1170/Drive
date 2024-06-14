8ae4f18658ab8db96e0d65a8577a93c338a60f3a
AtulTiwar1170 <atult2620@gmail.com>
2024-06-14 22:17:49 +0530

    setup env variable

--- config/mongoose.connection.js
+++ config/mongoose.connection.js
@@ -1,11 +1,16 @@
 const mongoose = require("mongoose");
+const config = require("config");
 
-mongoose.connect("mongodb://localhost:27017/scatch")
+const dbgr = require("debug")("development:mongoose");
+
+
+mongoose
+.connect(`${config.get("MONGODB_URI")}/scatch`)
 .then( function() {
-    console.log("connected");
+    dbgr("connected");
 })
 .catch( function(err) {
-    console.log("error occured during mongodb connection",err);
+    dbgr("error occured during mongodb connection",err);
 })
 
 module.exports = mongoose.connection;  
